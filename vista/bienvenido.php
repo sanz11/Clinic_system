81f4f1725f3e798bc4d837b53f2d421f9bf1ce12
Abel <abelricrasanchez@gmail.com>
2016-10-09 08:14:43 -0500

    actualizacion de bienvenido.php con sessiones

--- vista/bienvenido.php
+++ vista/bienvenido.php
@@ -1,5 +1,5 @@
 <div class="contenido">
    <h1 class="bien">
-       Bienvenido Juancho Carloncho, Buen Dia
+       Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellidos'];?>, Buen Dia
    </h1>
 </div>
