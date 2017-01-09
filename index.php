5ef72aaa0cbb0319b6ec5cc522f5966d80e433e1
Abel <abelricrasanchez@gmail.com>
2017-01-08 23:38:23 -0500

    actualize login

--- index.php
+++ index.php
@@ -1,94 +1,94 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
-  <style>
-    body {
-      display: flex;
-      min-height: 100vh;
-      flex-direction: column;
-    }
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Login | VETERINARIA</title>
+  
+  <link rel="stylesheet" href="css/login.css">
+  
+  <!-- CORE CSS-->
+   <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-    main {
-      flex: 1 0 auto;
-    }
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    body {
-      background: #fff;
-    }
 
-    .input-field input[type=date]:focus + label,
-    .input-field input[type=text]:focus + label,
-    .input-field input[type=email]:focus + label,
-    .input-field input[type=password]:focus + label {
-      color: #e91e63;
-    }
-
-    .input-field input[type=date]:focus,
-    .input-field input[type=text]:focus,
-    .input-field input[type=email]:focus,
-    .input-field input[type=password]:focus {
-      border-bottom: 2px solid #e91e63;
-      box-shadow: none;
-    }
-  </style>
+  
 </head>
 
-<body>
-  <div class="section"></div>
-  <main>
-    <center>
-      <img class="responsive-img" style="width: 250px;" src="http://i.imgur.com/ax0NCsK.gif" />
-      <div class="section"></div>
+<body class="light-blue">
+ <?php include 'controlador/login.php';?>
 
-      <h5 class="indigo-text">Please, login into your account</h5>
-      <div class="section"></div>
 
-      <div class="container">
-        <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
-
-          <form class="col s12" method="post">
-            <div class='row'>
-              <div class='col s12'>
-              </div>
-            </div>
-
-            <div class='row'>
-              <div class='input-field col s12'>
-                <input class='validate' type='email' name='email' id='email' />
-                <label for='email'>Enter your email</label>
-              </div>
-            </div>
-
-            <div class='row'>
-              <div class='input-field col s12'>
-                <input class='validate' type='password' name='password' id='password' />
-                <label for='password'>Enter your password</label>
-              </div>
-              <label style='float: right;'>
-								<a class='pink-text' href='#!'><b>Forgot Password?</b></a>
-							</label>
-            </div>
-
-            <br />
-            <center>
-              <div class='row'>
-                <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect indigo'>Login</button>
-              </div>
-            </center>
-          </form>
+  <div id="login-page" class="row ">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form" action="?" method="post">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <img src="im/logo2.jpg" alt="" class="responsive-img valign profile-image-login">
+            <p class="center login-form-text">Inicio de sesión</p>
+          </div>
         </div>
-      </div>
-      <a href="#!">Create account</a>
-    </center>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="prefix material-icons">perm_identity</i>
+            <input class="validate" id="email" name="user" type="text">
+            <label for="email" data-error="Escribe un correo válido" data-success="right" >Email</label>
+          </div>
+        </div>
+		
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock</i>
+            <input id="password" name="pass" type="password">
+            <label for="password">Contraseña</label>
+          </div>
+        </div>
+        <!--
+        <div class="row">          
+          <div class="input-field col s12 m12 l12  login-text">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+          </div>
+        </div>-->
+        <div class="row">
+          <div class="input-field col s12">
+             <input class="btn waves-effect  light-blue darken-2 col s12" type="submit" name="ingresar" value="Entrar">
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 m6 l6">
+            <p class="margin medium-small"><a href="register.html">Registrar</a></p>
+          </div>
+          <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="forgot-password.html">Recuperar mi contraseña</a></p>
+          </div>          
+        </div>
 
-    <div class="section"></div>
-    <div class="section"></div>
-  </main>
+      </form>
+    </div>
+  </div>
 
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+  <!-- jQuery Library -->
+ <script type="text/javascript" src="js/jquery.js"></script>
+  <!--materialize js-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+
+  
+  
+   <footer class="page-footer transparent">
+          <div class="footer-copyright  h light-blue darken-3">
+            <div class="container center">
+            © 2017 designed by
+            <a class="white-text " href="">codeAB</a>
+            </div>
+          </div>
+  </footer>
 </body>
 
 </html>
+
